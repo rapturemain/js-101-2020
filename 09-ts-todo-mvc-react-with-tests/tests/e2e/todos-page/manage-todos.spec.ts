7e85b4863ed2565472173a73f78d61100fb632e6
Rapture <rapture.main@gmail.com>
2021-03-29 22:37:18 +0300

    Cypress deletion check

--- 09-ts-todo-mvc-react-with-tests/tests/e2e/todos-page/manage-todos.spec.ts
+++ 09-ts-todo-mvc-react-with-tests/tests/e2e/todos-page/manage-todos.spec.ts
@@ -55,4 +55,42 @@ describe('Manage Todos', () => {
       cy.get('[data-test-id=todo-item__remove-action]').should('be.visible');
     });
   });
+
+  context('Creation and deletion', () => {
+    it('Creation and deletion', () => {
+      cy.fixture('todo-item.response.json').then((todoItemResponse)  => {
+        cy.intercept(
+            'POST',
+            todosEndpoint,
+            req => {
+              const { body } = req;
+              req.reply({
+                statusCode: 200,
+                body: JSON.stringify({
+                  ...todoItemResponse,
+                  text: body.text
+                })
+              });
+            }
+        ).as('createTodo');
+      });
+
+      const itemText = Math.random().toString() + '_' + Date.now();
+
+      authorize();
+      cy.get('[data-test-id=create-new-todo-form]').should('be.visible');
+      cy.get('[data-test-id=create-new-todo-form__todo-text-input]').type(itemText);
+      cy.get('[data-test-id=create-new-todo-form]').submit();
+
+      cy.wait('@createTodo');
+      // Check if creation were successful
+      cy.get('[data-test-id=todos-list]').should('be.visible');
+      cy.get('[data-test-id=todo-item]').should('be.visible');
+      cy.get('[data-test-id=todo-item__checked-checkbox]').should('not.be.checked');
+      cy.get('[data-test-id=todo-item__text-input]').should('have.value', itemText)
+      // Delete after creation check passed
+      cy.get('[data-test-id=todo-item__remove-action]').click({force: true})
+      cy.get('[data-test-id=todo-item]').should('not.exist');
+    });
+  });
 });
